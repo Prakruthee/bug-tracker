73d0558c40acfb865f894aaa0a0c7f3e5cac70d7
prakruthee <psudanandappa@qwinixtech.com>
2015-03-04 10:19:32 +0530

    added changes in  facebook authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,21 +2,25 @@ class User < ActiveRecord::Base
 
 
   has_many :issues
-  has_secure_password
+  has_secure_password :validations => false
 
-  validates :name, presence: true
+  validates :name, presence: true,
+            :unless => proc{|u| u.provider.present?}
 
   validates :username, presence: true,
             uniqueness: true,
             :length => {:minimum => 6, :maximum => 256},
-            :format => {:with => /\A[0-9\_\.]*[a-zA-Z]+[0-9\_\.]*\z/ }
+            :format => {:with => /\A[0-9\_\.]*[a-zA-Z]+[0-9\_\.]*\z/ },
+            :unless => proc{|u| u.provider.present?}
 
   validates :email, presence: true,
             uniqueness: true,
-            :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+            :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
+            :unless => proc{|u| u.provider.present?}
 
   validates :password, :presence => true,
-           :length => {:minimum => 6}
+            :length => {:minimum => 6},
+            :unless => proc{|u| u.provider.present?}
 
   def self.authenticate(username, password)
     user = find_by_username(username)
