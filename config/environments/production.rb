607c691d194ebe9a7f40de9a07386158bf88a721
prakruthee <psudanandappa@qwinixtech.com>
2015-03-11 13:14:27 +0530

    Added sendgrid functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,17 +79,20 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   Rails.application.configure do
+    config.action_mailer.asset_host = 'https://bugtrackers.herokuapp.com'
+    config.action_mailer.default_url_options = { :host => 'bugtrackers.herokuapp.com' }
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default :charset => "utf-8"
 
-      config.action_mailer.default_url_options ={:host => "https://bugtrackers.herokuapp.com"}
-        ActionMailer::Base.smtp_settings = {
-        :address    => "smtp.gmail.com",
-        :port       => 587,
-        :domain     => "gmail.com",
-        :user_name  => "prakruthis320@gmail.com",
-        :password   => "prakruthis20",
-        :authentication   => "plain",
-        :enable_starttls_auto => true
-   }
-end
-
+    config.action_mailer.smtp_settings = {
+      :enable_starttls_auto => true,
+      :address => "smtp.sendgrid.net",
+      :port => 587,
+      :domain => "sendgrid.com",
+      :authentication => :login,
+      :user_name =>"prakruthis.20",
+      :password => "prakruthis20"
+    }
+  end
 end
