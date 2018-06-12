90ab997da3358653d13da295f77bc449b1bd44b1
nikhil405 <npraksh@qwinix.io>
2018-06-12 17:16:00 +0530

    deploy the application

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,45 +1,23 @@
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary server in each group
-# is considered to be the first unless any hosts have the primary
-# property set.  Don't declare `role :all`, it's a meta role.
-
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
-
+set :stage, :production
+set :branch, :demo_deploy2
+set :rails_env, :it
+set :deploy_to, '/u01/apps/qwinix/bugtrackr'
+set :log_level, :debug
 
 # Extended Server Syntax
 # ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a hash can be used to set
+# extended properties on the server.
+role :app, %w{deploy@18.216.211.144}
+role :web, %w{deploy@18.216.211.144}
+role :db, %w{deploy@18.216.211.144}
+server '18.216.211.144', roles: %w{:web, :app, :db}, user: 'deploy'
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# And/or per server (overrides global)
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+set :ssh_options, {
+   #verbose: :debug,
+   keys: %w(~/.ssh/id_rsa),
+   auth_methods: %w(publickey)
+}
